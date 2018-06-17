4ff4edfe3544b465ffaff3ec3c2e4c2e407a6727
Giruvagen <40126586+Giruvagen@users.noreply.github.com>
2018-06-17 19:22:09 +0100

    added getnames function to pull system name data

--- RouteRequests.py
+++ RouteRequests.py
@@ -4,11 +4,11 @@ dest = str(input('Ending system:'))
 def getroute(ori, dest):
   import requests
   import string
-  url="https://esi.evetech.net/latest/route/O/D/?datasource=tranquility&flag=shortest"
-  url = url.replace('O', ori)
-  url = url.replace('D', dest)
-  print(url)
-  response = requests.get(url, verify=True)
+  urlR="https://esi.evetech.net/latest/route/O/D/?datasource=tranquility&flag=shortest"
+  urlR = urlR.replace('O', ori)
+  urlR = urlR.replace('D', dest)
+  print(urlR)
+  response = requests.get(urlR, verify=True)
   if response.status_code != 200:
     print('Status:', response.status_code, 'Problem with the request. Exiting.')
     exit()
@@ -16,3 +16,11 @@ def getroute(ori, dest):
   print(data)
   return data
 getroute(ori, dest)
+def getnames(ori,dest):
+  import json
+  import requests
+  urlN = "https://esi.evetech.net/latest/universe/names/?datasource=tranquility"
+  dataN = json.dumps([ori,dest])
+  N = requests.post(url = urlN, data = dataN)
+  print(N.text)
+getnames(ori, dest)
