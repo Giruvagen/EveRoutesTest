8d35b741c868ece5acf7b1bb504a4bfff097df75
Giruvagen <40126586+Giruvagen@users.noreply.github.com>
2018-06-17 12:57:02 +0100

    accept user input, print and return list

--- reuqests.py
+++ reuqests.py
@@ -1,10 +1,18 @@
 #request api data using normal requests module rather than esipy
+ori = str(input('Starting system:'))
+dest = str(input('Ending system:'))
 def getroute(ori, dest):
   import requests
-  url="https://esi.evetech.net/latest/route/30000142/30002187/?datasource=tranquility&flag=shortest"
+  import string
+  url="https://esi.evetech.net/latest/route/O/D/?datasource=tranquility&flag=shortest"
+  url = url.replace('O', ori)
+  url = url.replace('D', dest)
+  print(url)
   response = requests.get(url, verify=True)
   if response.status_code != 200:
-  print('Status:', response.status_code, 'Problem with the request. Exiting.')
-  exit()
+    print('Status:', response.status_code, 'Problem with the request. Exiting.')
+    exit()
   data = response.json()
+  print(data)
   return data
+getroute(ori, dest)
